74b7a564a754fa6a3a614a8a83c370df6b0d03b9
lavanya-sekar <lavanya.sekar003@gmail.com>
2019-07-21 13:51:12 +0530

    Add files via upload

---
+++ sample1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class sample1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		System.out.println("Enter n value");
+		BufferedReader b=new BufferedReader(new InputStreamReader(System.in));
+		n=Integer.parseInt(b.readLine());
+		if (n>0)
+		{
+			System.out.println("Positive");
+		}
+		else if (n<0)
+		{
+			System.out.println("Negative");
+		}
+		else 
+		{
+			System.out.println("Zero");
+		}
+	}
+}
